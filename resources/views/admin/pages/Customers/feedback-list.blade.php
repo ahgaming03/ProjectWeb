40e194475e32fabf459a607ba4f6c5451fa33909
Cuongs <dangcuong9331@gmail.com>
2023-08-03 00:02:12 +0700

    Customerfeedback
    Feedback

--- resources/views/admin/pages/Customers/feedback-list.blade.php
+++ resources/views/admin/pages/Customers/feedback-list.blade.php
@@ -7,15 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>GamingGear Admin</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="{{ asset('admin/vendors/ti-icons/css/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('admin/vendors/base/vendor.bundle.base.css') }}">
+    <link rel="stylesheet" href="{{ asset('admjn/vendors/ti-icons/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('admjn/vendors/base/vendor.bundle.base.css') }}">
     <!-- endinject -->
     <!-- plugin css for this page -->
     <!-- End plugin css for this page -->
     <!-- inject:css -->
-    <link rel="stylesheet" href="{{ asset('admin/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('admjn/css/style.css') }}">
     <!-- endinject -->
-    <link rel="shortcut icon" href="{{ asset('admin/images/favicon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('admjn/images/favicon.png') }}" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 
@@ -33,8 +33,37 @@
                     <div class="col-lg-12 grid-margin stretch-card">
                         <div class="card">
                             <div class="card-body">
-                                <h4 class="card-title">Product list</h4>
-
+                                <h4 class="card-title">Feedback List</h4>
+                                <div class="table-responsive">
+                                    <table class="table table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th>Feedback ID</th>
+                                                <th>CustomerID</th>                                                                             
+                                                <th>ProductID</th>
+                                                <th>Rating</th>                                               
+                                                <th>Comment</th>
+                                                <th>Status</th>
+                                                <th>Update at</th>
+                                                <th>Create at</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach($feedbacks as $fb)
+                                            <tr>
+                                                <td>{{ $fb->feedbackID }}</td>
+                                                <td>{{ $fb->customerID }}</td>                                                                               
+                                                <td>{{ $fb->productID }}</td>
+                                                <td>{{ $fb->rating }}</td>                                     
+                                                <td>{{ $fb->comment }}</td>
+                                                <td>{{ $fb->is_status }}</td>
+                                                <td>{{ $fb->update_at }}</td>
+                                                <td>{{ $fb->create_at }}</td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>

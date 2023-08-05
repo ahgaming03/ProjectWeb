2206ab1b36bd24f5c793cbfcfd0ee85cff5139ba
thinhgcs210935 <129242283+thinhgcs210935@users.noreply.github.com>
2023-08-05 17:00:34 +0700

    product-add

--- resources/views/admin/pages/products/product-add.blade.php
+++ resources/views/admin/pages/products/product-add.blade.php
@@ -27,43 +27,52 @@
                                         name="name">
                                 </div>
                             </div>
-                            <div>
+                            <div class="aPrice">
                                 <div class="mb-3">
                                     <label for="price">Price:</label>
                                     <input type="number" class="form-control" id="price" placeholder="Enter Product Price"
                                         name="price">
                                 </div>
                             </div>
-                            
+                            <div class="aCat">
+                                <div class="mb-3">
+                                    <label for="category">Category:</label>
+                                    <select name="category" id="category" class="form-control">
+                                        @foreach ($cat as $cat)
+                                            <option value="{{ $cat->catID }}">{{ $cat->catName }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
                             <div class="mb-3">
                                 <label for="image">Image:</label>
                                 <input type="file" class="form-control" id="image" placeholder="Enter Product Image"
                                     name="image">
                             </div>
-                            <div class="mb-3 mt-3">
-                                <label for="details">Details:</label>
-                                <textarea class="form-control" rows="5" id="details" placeholder="Enter Product Details" name="details"></textarea>
-                            </div>
-                            <div class="mb-3">
-                                <label for="category">Category:</label>
-                                <select name="category" id="category" class="form-control">
-                                    @foreach ($cat as $cat)
-                                        <option value="{{ $cat->catID }}">{{ $cat->catName }}</option>
-                                    @endforeach
-                                </select>
+                            <div class="aDetail">
+                                <div class="mb-3 mt-3">
+                                    <label for="details">Details:</label>
+                                    <textarea class="form-control" rows="5" id="details" placeholder="Enter Product Details" name="details"></textarea>
+                                </div>
                             </div>
                             <button type="submit" class="btn btn-primary">Submit</button>
                             <a href="{{ url('product-list') }}" class="btn btn-primary">Back</a>
                         </form>
                         <style>
                             .aID{
-                                width:45%;
-                                position: absolute;
+                                width:35%;
                             }
                             .aName{
-                                width:45%;
-                                position: absolute;
-                                margin-top: ;
+                                width:35%;
+                            }
+                            .aPrice{
+                                width:35%;
+                            }
+                            .aPrice{
+                                width:35%;
+                            }
+                            .aCat{
+                                width:35%;
                             }
                         </style>
                     </div>
